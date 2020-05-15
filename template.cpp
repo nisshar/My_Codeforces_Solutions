c970a9d710339ebbd6e79863d98d5b241bbe446d
Nishant Sharma <54657882+nisshar@users.noreply.github.com>
2020-05-15 15:08:07 +0000

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> 
+using namespace std;
+#define ll long long int
+#define a_input(n,arr) ll n; cin>>n; ll arr[n]; for(ll i=0;i<n;i++) cin>>arr[i];
+#define a_print for(ll i=0;i<n;i++) cout<<arr[i]<<" ";
+#define endl "\n"
+#define w(x) int x; cin>>x; while(x--) 
+int main(){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
