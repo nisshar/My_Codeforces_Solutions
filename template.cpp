f1bc30648fd6269b5d4461f3cc4445b53eca56f6
Nishant Sharma <54657882+nisshar@users.noreply.github.com>
2020-05-28 16:07:34 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,15 +1,33 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define a_input(n,arr) ll n; cin>>n; ll arr[n]; for(ll i=0;i<n;i++) cin>>arr[i];
-#define a_print for(ll i=0;i<n;i++) cout<<arr[i]<<" ";
+#define int long long
+#define f first
+#define s second
+#define pb push_back
+#define a_input(n,arr) int n; cin>>n; int arr[n]; for(int i=0;i<n;i++) cin>>arr[i];
+#define a_print(n,arr) for(int i=0;i<n;i++) cout<<arr[i]<<" ";
 #define endl "\n"
 #define w(x) int x; cin>>x; while(x--) 
 #define vi vector<int> 
+#define pi pair<int,int>
 #define umap unordered_map
-int main(){
-    
-   
-   
-    return 0;
+void solve(){
+	int n,k,e=0,bit=0;
+	
+}
+void Input()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int32_t main(){
+	Input();
+	w(t){
+		solve();
+	}
+
+	return 0;
 }
